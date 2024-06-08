b775c7cf21cf05b9039340508e40cd1a3e35f460
LianShaoKang <shaokanglsk@gmail.com>
2024-06-08 22:37:21 +0800

    start-up page

--- NOMsFood/app/(tabs)/index.tsx
+++ NOMsFood/app/(tabs)/index.tsx
@@ -1,70 +1,76 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, Platform, useColorScheme } from 'react-native';
+//import { HelloWave } from '@/components/HelloWave';
+//import ParallaxScrollView from '@/components/ParallaxScrollView';
+//import { ThemedText } from '@/components/ThemedText';
+//import { ThemedView } from '@/components/ThemedView';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function HomeScreen() {
+  const colorScheme = useColorScheme() ?? 'light';
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={[styles.container, { backgroundColor: '#FFFFFF' }]}>
+      <Image
+        source={require('../../assets/images/nomsicon.png')}
+        style={[styles.logo]}
+      />
+      <Text style={[styles.title]}>Welcome to NOMs Food!</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          style={[styles.button,]}
+          onPress={() => console.log('Login')}>
+          <Text style={[styles.buttonText]}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button]}
+          onPress={() => console.log('Sign in')}>
+          <Text style={[styles.buttonText]}>Sign in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button]}
+          onPress={() => console.log('Forgot password')}>
+          <Text style={[styles.buttonText]}>
+            Forgot password
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#10390A',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+    color: '#000000',
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    color: '#000000',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
